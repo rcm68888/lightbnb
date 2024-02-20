723d1a95d896261b7e08f333619b8723a305b128
Rcm <rcm68888@gmail.com>
2024-02-20 09:47:01 -0700

    refactoring database.js with SQL queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,6 +1,15 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 /// Users
 
 /**
@@ -9,15 +18,17 @@ const users = require("./json/users.json");
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  let userEmail = email.toLowerCase();
+  return pool.query(`
+  SELECT * FROM users
+  WHERE users.email = $1
+  ;`, [userEmail])
+  .then(res =>  res.rows[0])
+  .catch((err) => {
+    console.error('query error', err.stack)
+  })
 };
+exports.getUserWithEmail = getUserWithEmail;
 
 /**
  * Get a single user from the database given their id.
@@ -25,8 +36,16 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+  SELECT * FROM users
+  WHERE users.id = $1
+  ;`, [id])
+  .then(res => res.rows[0])
+  .catch((err) => {
+    console.error('query error', err.stack)
+  })
 };
+exports.getUserWithId = getUserWithId;
 
 /**
  * Add a new user to the database.
@@ -34,11 +53,18 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *; 
+  ;`, [user.name, user.email, user.password]) 
+  //Database Query
+  .then(res =>  res.rows[0])
+  .catch((err) => {
+    console.error('query error', err.stack)
+  })
 };
+exports.addUser = addUser;
 
 /// Reservations
 
